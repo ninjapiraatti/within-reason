39ab5df911a686e89d327d99e3019e1fc18bd6bc
ninjapiraatti <tuoppil@gmail.com>
2023-09-13 08:15:49 +0200

    Change the example unit test to reflect project better

---
+++ app/src/components/__tests__/ProjectItem.spec.ts
@@ -0,0 +1,31 @@
+import { describe, it, expect } from "vitest"
+
+import { mount } from "@vue/test-utils"
+import ProjectItem from "@/components/ProjectItem.vue"
+
+describe("Project Item", () => {
+	it("renders properly", () => {
+		const wrapper = mount(ProjectItem, {
+			global: {
+				stubs: {
+					"router-link": true,
+				},
+			},
+			props: { 
+				project: {
+					id: 1,
+					attributes: {
+						title: "Test Project",
+						ingress: "Lorem",
+						body: "Ipsum",
+						image: "/imagepath/image.jpg",
+						video: "/videopath/video.mp4",
+					},
+					imageUrl: "/imagepath/image.jpg",
+					meta: {},
+				},
+			},
+		})
+		expect(wrapper.text()).toContain("Test Project")
+	})
+})
