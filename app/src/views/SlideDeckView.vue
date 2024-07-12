d41dd65d9d1e3009ef7c0081c8daef2608403d04
Tuomas Louekari <tuoppil@gmail.com>
2024-07-12 21:44:37 +0300

    Add basic structure for slide deck

---
+++ app/src/views/SlideDeckView.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="overflow-hidden">
+		<Transition name="slide-fade" mode="out-in">
+			<component :is="slides[currentSlide]" @next="nextSlide" :key="currentSlide"></component>
+		</Transition>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue"
+import SlideA from "@/components/SlideA.vue"
+
+const slides = [SlideA /* , OtherSlide, AnotherSlide, etc. */]
+const currentSlide = ref(0)
+
+const nextSlide = () => {
+	if (currentSlide.value < slides.length - 1) {
+		currentSlide.value++
+	}
+}
+</script>
+
+<style scoped>
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+	transition: all 0.5s ease;
+}
+
+.slide-fade-enter-from {
+	transform: translateX(100%);
+	opacity: 0;
+}
+
+.slide-fade-leave-to {
+	transform: translateX(-100%);
+	opacity: 0;
+}
+</style>
