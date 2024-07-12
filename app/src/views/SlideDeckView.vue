258539d668a55228df040be015ab5ba2b8746ade
Tuomas Louekari <tuoppil@gmail.com>
2024-07-12 23:08:07 +0300

    Try to implement swipe

--- app/src/views/SlideDeckView.vue
+++ app/src/views/SlideDeckView.vue
@@ -1,10 +1,9 @@
 <template>
-	<div class="overflow-hidden">
+	<div class="slide-container relative overflow-hidden h-screen w-screen" ref="swipeTarget">
 		<Transition name="slide-fade">
 			<component
 				:is="slides[currentSlide]"
 				@next="nextSlide"
-				@first="currentSlide = 0"
 				:key="currentSlide"
 				class="absolute top-0 left-0 w-full h-full"
 			></component>
@@ -13,24 +12,53 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue"
+import { ref, onMounted } from "vue"
+import { useSwipe } from "@vueuse/core"
 import SlideA from "@/components/SlideA.vue"
 import SlideB from "@/components/SlideB.vue"
+// Import other slides here
 
 const slides = [SlideA, SlideB]
 const currentSlide = ref(0)
+const swipeTarget = ref<HTMLElement | null>(null)
 
 const nextSlide = () => {
 	if (currentSlide.value < slides.length - 1) {
 		currentSlide.value++
 	}
 }
+
+const previousSlide = () => {
+	if (currentSlide.value > 0) {
+		currentSlide.value--
+	}
+}
+
+onMounted(() => {
+	if (!swipeTarget.value) {
+		console.log("Swipe target not found")
+		return
+	}
+	const { isSwiping, direction } = useSwipe(swipeTarget, {
+		onSwipe(e) {
+			console.log("event")
+			if (direction.value === "left") {
+				console.log("left")
+				nextSlide()
+			} else if (direction.value === "right") {
+				console.log("right")
+				previousSlide()
+			}
+		},
+	})
+})
 </script>
 
 <style scoped>
 .slide-container {
-	background-color: #092f3f; /* Set this to match your slides' background */
+	background-color: #ffffff; /* Set this to match your slides' background */
 }
+
 .slide-fade-enter-active,
 .slide-fade-leave-active {
 	transition: all 0.5s ease;
@@ -38,9 +66,11 @@ const nextSlide = () => {
 
 .slide-fade-enter-from {
 	transform: translateX(100%);
+	opacity: 0;
 }
 
 .slide-fade-leave-to {
 	transform: translateX(-100%);
+	opacity: 0;
 }
 </style>
